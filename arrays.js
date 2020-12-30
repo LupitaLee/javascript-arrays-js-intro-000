9b7c7f5bf11cc187061e9427b0849af660bc1b60
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-30 22:29:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,2 @@
 var chocolateBars = ["snickers", "hundred grand","kitkat", "skittles"];
-function addElementToBeginningOfArray(array, element){return [element,...array];
-}
-function destructivelyAddElementToBeginningOfArray(array, element)
-{array.unshift(element);return array}
-
-function addElementToEndOfArray(array, element){array.push(element);return array}
-
-function destructivelyAddElementToBeginningOfArrayarray, (array, element ){return [...array,element];
 }
