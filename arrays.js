5763b3483c2c83825c4a085ab477141f450f3edb
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-30 22:22:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,6 @@ function addElementToBeginningOfArray(array, element){return [element,...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element)
 {array.unshift(element);return array}
+
+function addElementToEndOfArray(array, element){aaray.push(element);
+return array}
