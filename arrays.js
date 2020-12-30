371b4441cfdbb44e9d0b17a7d1fb7dddaa179fb6
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-30 23:07:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,5 @@ function accessElementInArray(array, index){
 
 function destructivelyRemoveElementFromBeginningOfArray(array){array.shift()
 return array}
+
+function removeElementFromBeginningOfArray(array){return array.slice(1)}
