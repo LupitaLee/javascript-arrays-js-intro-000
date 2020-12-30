4ba6404a138f80caa98e44fcc196b7f8efb71a51
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-30 23:20:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,4 +22,6 @@ function removeElementFromBeginningOfArray(array){return array.slice(1)}
 function destructivelyRemoveElementFromEndOfArray(array){array.pop()
 return array}
 
-function removeElementFromEndOfArray(array)
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
